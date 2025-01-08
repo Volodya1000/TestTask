6945f74ece37a738d3873e8e85e6b919a5d76f03
volodya <volodya.dichkovski@gmail.com>
2025-01-08 19:37:16 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Воозвращает автора, который написал книгу с самым длинным названием
+        //в случае если таких авторов окажется несколько, возвращает автора с наименьшим Id
+        public async Task<Author> GetAuthor()
+        {
+            return await _context.Books
+                .OrderByDescending(b => b.Title.Length)
+                .ThenBy(b => b.AuthorId)
+                .Select(b => b.Author)
+                .FirstOrDefaultAsync();
+        }
+
+        //Возвращает авторов, написавших четное количество книг, изданных после 2015 года
+        public async Task<List<Author>> GetAuthors()
+        {
+            return await _context.Authors
+                    .AsNoTracking()
+                    .Where(a => a.Books.Any(b => b.PublishDate.Year > 2015) &&
+                            a.Books.Count(b => b.PublishDate.Year > 2015) % 2 == 0)
+                    .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        
+        private static readonly DateTime CarolusRexReleaseDate = new DateTime(2012, 5, 25);
+
+        private readonly ApplicationDbContext _context;
+
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Возвращает книгу с наибольшей стоимостью опубликованного тиража
+        public async Task<Book> GetBook()
+        {
+            return await _context.Books
+                .AsNoTracking()
+                .OrderByDescending(b=>b.Price*b.QuantityPublished)
+                .FirstOrDefaultAsync();
+        }
+
+        //Возвращает книги, в названии которой содержится "Red" и которые опубликованы после выхода альбома "Carolus Rex" группы Sabaton
+        public async Task<List<Book>> GetBooks()
+        {
+            // Дата выхода альбома "Carolus Rex"
+            var carolusRexReleaseDate = new DateTime(2012, 5, 25);
+
+            return await _context.Books
+                .AsNoTracking()
+                .Where(b=>b.Title.Contains("Red") && b.PublishDate > CarolusRexReleaseDate)
+                .ToListAsync();
+        }
+    }
+}
